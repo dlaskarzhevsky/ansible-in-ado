dfb2232095f9309a4eafb3bbac4c2783f85363e0
Daniel <daniel.laskarzhevsky@gmail.com>
2025-01-02 20:09:50 -0500

    Removed non-ascii characted from install-ansible.sh

--- create-ubuntu-vm-in-azure/install-ansible.sh
+++ create-ubuntu-vm-in-azure/install-ansible.sh
@@ -11,7 +11,7 @@ sudo apt -y install python3-pip
 # error: externally-managed-environment
 
 # × This environment is externally managed
-# ╰─> To install Python packages system-wide, try apt install
+# ─> To install Python packages system-wide, try apt install
 #     python3-xyz, where xyz is the package you are trying to
 #     install.
 
