c4ef8e21420b7f4fb53c4a1ab1065b5e06fffd0a
Daniel <daniel.laskarzhevsky@gmail.com>
2025-01-02 19:53:34 -0500

    Updated install-ansible.sh script

---
+++ create-ubuntu-vm-in-azure/install-ansible.sh
@@ -0,0 +1,19 @@
+# Update the repository package list
+sudo apt update
+
+# Install python3-pip module.
+# Automatic --yes -y --assume-yes to prompts
+sudo apt -y install python3-pip
+
+# Install Ansible
+# pip3 install ansible fails with 
+
+# error: externally-managed-environment
+
+# × This environment is externally managed
+# ╰─> To install Python packages system-wide, try apt install
+#     python3-xyz, where xyz is the package you are trying to
+#     install.
+
+# Install Ansible
+sudo apt -y install ansible
